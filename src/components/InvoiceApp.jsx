705e5fc10a9df9a76b90d2390f9bede81774c931
SiralexisStar <alexgarciavillar96@gmail.com>
2025-09-14 19:05:11 +0200

    First Commit.

---
+++ src/components/InvoiceApp.jsx
@@ -0,0 +1,13 @@
+import { invoice } from "../data/invoice";
+export const InvoiceApp = () => {
+
+  return (
+    <>
+      <h1>Ejemplo Factura</h1>
+      <ul>
+        <li>Id: {invoice.id}</li>
+        <li> </li>
+      </ul>
+    </>
+  );
+};
