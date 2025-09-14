a03813d4e6d2355ab2dce063cff117f99351a776
SiralexisStar <alexgarciavillar96@gmail.com>
2025-09-14 19:30:15 +0200

    destructuring the invoice Data

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -1,15 +1,26 @@
 import { getInvoice } from "../services/getInvoice";
 export const InvoiceApp = () => {
-    
+
   // With this function can obtain the data from the invoice.js
-  const invoice = getInvoice();
+  const {id, name, client, company, items} = getInvoice();
+  const { name:nameClient, lastName} = client;
+  const {country, city, street} = client.address;
 
   return (
     <>
       <h1>Ejemplo Factura</h1>
       <ul>
-        <li>Id: {invoice.id}</li>
-        <li>Name: {invoice.name} </li>
+        <li>Id: { id } </li>
+        <li>Name: { name } </li>
+      </ul>
+
+      <h3>Datos del cliente</h3>
+      <ul>
+        <li>Nombre del cliente: { nameClient } </li>
+        <li>Apellido: { lastName } </li>
+        <li>Pais: { country } </li>
+        <li>Ciudad: { city } </li>
+        <li>Calle: { street } </li>
       </ul>
     </>
   );
