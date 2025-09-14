add270ce3bb7183f32bdf9eb94d58e8cf8ac228d
SiralexisStar <alexgarciavillar96@gmail.com>
2025-09-14 19:14:01 +0200

    emulate ReactQuery with mocked data

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -1,12 +1,15 @@
-import { invoice } from "../data/invoice";
+import { getInvoice } from "../services/getInvoice";
 export const InvoiceApp = () => {
+    
+  // With this function can obtain the data from the invoice.js
+  const invoice = getInvoice();
 
   return (
     <>
       <h1>Ejemplo Factura</h1>
       <ul>
         <li>Id: {invoice.id}</li>
-        <li> </li>
+        <li>Name: {invoice.name} </li>
       </ul>
     </>
   );
